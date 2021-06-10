2698c808499f7507ae0570759e389ec979ed2162
Daniel Chung <dtchung8@gmail.com>
2021-06-10 14:04:00 -0400

    First Commit

--- PyPrintTest.py
+++ PyPrintTest.py
@@ -1,3 +1,9 @@
 print("Hello World!")
 n=input("What is your name?")
 print(n,", nice to meet you!")
+k=input("Enter a number: ")
+
+if int(k) > 10:
+    print("This is a big number!")
+else:
+    print("This is a small number")
