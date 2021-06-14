ea768d26eb5f8fcff270974d0719069113026043
Daniel Chung <dtchung8@gmail.com>
2021-06-14 16:19:30 -0400

    double changes

---
+++ car.py
@@ -0,0 +1,7 @@
+class car:
+
+    def __init__(self, name, speed, horsepower):
+        self.name = name
+        self.speed = speed
+        self.horsepower = horsepower
+
---
+++ carlist.py
@@ -0,0 +1,9 @@
+from car import car
+
+Buick = car("Buick", "80mph", "400 horsepower")
+Tesla = car("Tesla", "120mph", "200 horsepower")
+cars = []
+cars.append(Buick)
+cars.append(Tesla)
+
+print(cars[1])
